d94976859f29614f9b45809893d218a13e2ba0ce
mtahabinrashid <muhammadtahabinrashid@gmail.com>
2025-07-23 02:24:20 +0500

    home page

--- index.php
+++ index.php
@@ -1,6 +1,154 @@
 <?php $title = "Home";
 include 'include/header.php' ?>
 
+<section class="main_banner padd-y">
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="main_banner_content text-center">
+                    <figure class="main_banner_img">
+                        <img src="images/banner-logo.png" alt="image" class="img-fluid">
+                    </figure>
+                    <h1>
+                        ELEVATE YOUR BRAND WITH FLASHY BESPOKE
+                    </h1>
+                    <p>
+                        Book a package with one click, or brief us on your custom photoshoot - the choice is yours.
+                    </p>
+                    <div>
+                        <a href="" class="themeBtn mx-auto">Book Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
+<section class="service_section padd-y">
+    <div class="container">
+        <div class="service_title mb-4">
+            <h2 class="heading">Our Services</h2>
+        </div>
+        <div class="row">
+            <div class="col-md-3">
+                <div class="service_card">
+                    <figure class="service_card_img">
+                        <img src="images/service-img1.png" alt="image" class="img-fluid">
+                    </figure>
+                    <h4>Product Photography</h4>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="service_card">
+                    <figure class="service_card_img">
+                        <img src="images/service-img2.jpg" alt="image" class="img-fluid">
+                    </figure>
+                    <h4>Food Photography</h4>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="service_card">
+                    <figure class="service_card_img">
+                        <img src="images/service-img3.jpg" alt="image" class="img-fluid">
+                    </figure>
+                    <h4>Real Estate Photography</h4>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="service_more">
+                    <h4>and more:</h4>
+                    <ul>
+                        <li><a href="">Event Photography</a></li>
+                        <li><a href="">Drone Photography</a></li>
+                        <li><a href="">Videography</a></li>
+                        <li><a href="">Virtual Tours</a></li>
+                        <li><a href="">Corporate Portraits</a></li>
+                        <li><a href="">Lifestyle Photography</a></li>
+                    </ul>
+                    <div>
+                        <a href="" class="themeBtn">Get Started</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="video_section padd-y">
+    <div class="container">
+        <iframe width="100%" height="800" src="https://www.youtube.com/embed/D0UnqGm_miA?si=ry9OpxN8UjK0OBBL"
+            title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+    </div>
+</section>
+
+<section class="quality_section padd-y">
+    <div class="container">
+        <div class="row align-items-center justify-content-between">
+            <div class="col-md-5">
+                <div class="quality_content">
+                    <h2 class="heading">WE understand quality CONTENT</h2>
+                    <h3 class="subHeading">SHOWCASE YOUR BUSINESS</h3>
+                    <p>
+                        From food photography to product photography, we are the experts.
+                    </p>
+                    <p>
+                        Guaranteed high-quality and market-ready content.
+                    </p>
+                    <h3 class="subHeading">TAKE IT EASY 💅</h3>
+                    <p>
+                        No more cancelled shoots and bad edits.
+                    </p>
+                    <p>
+                        We'll match you with an industry-leading professional photographer. All images are processed
+                        using
+                        AI and delivered within record time.
+                    </p>
+                    <div>
+                        <a href="" class="themeBtn">Book Now</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <figure class="quality_img">
+                    <img src="images/quality-img1.jpg" alt="image" class="img-fluid">
+                </figure>
+            </div>
+        </div>
+        <div class="row align-items-center justify-content-between flex-row-">
+            <div class="col-md-6">
+                <div class="quality_content">
+                    <h2 class="heading">SOMETHING SPECIAL?</h2>
+                    <h3 class="subHeading">SHOWCASE YOUR BUSINESS</h3>
+                    <p>
+                        Anything you want captured, we can deliver. Anywhere in the world 🌎
+                    </p>
+                    <p>
+                        We are ready to meet even the most challenging videography and photography requirements,
+                        globally.
+                    </p>
+                    <h3 class="subHeading">TAKE IT EASY 💅</h3>
+                    <p>
+                        No more cancelled shoots and bad edits.
+                    </p>
+                    <p>
+                        We'll match you with an industry-leading professional photographer. All images are processed
+                        using
+                        AI and delivered within record time.
+                    </p>
+                    <div>
+                        <a href="" class="themeBtn">Let’s Talk</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-6">
+                <figure class="quality_img">
+                    <img src="images/quality-img2.png" alt="image" class="img-fluid">
+                </figure>
+            </div>
+        </div>
+    </div>
+</section>
 
 <?php include 'include/footer.php' ?>
